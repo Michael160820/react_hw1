281f6d07ba389a39b2f12df46f031a1e7c5d7f19
Michael160820 <belyayevm@gmail.com>
2023-07-11 18:16:15 +0300

    hw 2 part 1 done

--- src/App.js
+++ src/App.js
@@ -1,3 +1,8 @@
+// з jsonplaceholder отримати всі пости в компоненту Posts.js
+// відобразити кожного інформацію (id,title) з кожного поста (компонента Post)
+// Зробити кнопку вибора поста, при натисканні на яку в Posts.js ви покажете детальну інфомацію про пост(всю інфу)
+
+
 import React from 'react';
 import {Posts} from "./components/Posts/Posts";
 
