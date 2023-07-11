181b845583065d0dd4bf61412509645b3f497048
Michael160820 <belyayevm@gmail.com>
2023-07-11 17:31:55 +0300

    Initialize project using Create React App

--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-const Post = () => {
+const Post = ({post}) => {
+    const {isShow, setIsShow} = useState(false);
+    const {id, title} = post;
     return (
         <div>
-            
+            <div>id: {id}</div>
+            <div>title: {title}</div>
+            <button onClick={()=>setIsShow(prev=>!prev)}>{isShow?'Hide':'Show'}</button>
         </div>
     );
 };
 
-export default Post;
+export {Post};
